b552c78b73417f90db90dea36a2d72bd28f2e3f7
codacy20 <kmuc72@gmail.com>
2019-07-09 17:40:42 +0200

    init

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <footer id="footer">
+            <p className="copyright">&copy; Untitled footer.</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/Header.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import './Header.css';
+
+// export interface Props{
+
+//     name: string;
+// }
+
+function Header() {
+
+    return (
+        <header id="header">
+            <div className="logo">
+                <span className="icon fa-gem"></span>
+            </div>
+            <div className="content">
+                <div className="inner">
+                    <h1>AmirK</h1>
+                    <p>this is just some text <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                </div>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#intro">Intro</a></li>
+                    <li><a href="#work">Work</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#elements">Elements</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
