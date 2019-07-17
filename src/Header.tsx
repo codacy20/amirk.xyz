aa8b5663372cb6e9c9351facb79e7cea4b5121c0
codacy20 <kmuc72@gmail.com>
2019-07-17 15:34:39 +0200

    fonts and more csss

--- src/Header.tsx
+++ src/Header.tsx
@@ -12,25 +12,25 @@ function Header() {
         <header id="header">
             <nav>
                 <ul>
-                    <li><a href="#intro">Intro</a></li>
-                    <li><a href="#work">Work</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#intro">ABOUT</a></li>
+                    <li><a href="#work">WORK</a></li>
+                    <li><a href="#about">ARTICLES</a></li>
+                    {/* <li><a href="#contact">Contact</a></li> */}
                     {/* <li><a href="#elements">Elements</a></li> */}
                 </ul>
             </nav>
             <div className="content">
                 <div className="inner">
-                    <h1>AmirK</h1>
-                    <p>this is just some text <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                    <h1>Amir Kiumarsi</h1>
+                    <p>DEVELOPER, FREELANCER and CLOUD ENGINEER</p>
                 </div>
             </div>
             <nav>
                 <ul>
-                    <li><a href="#intro">Intro</a></li>
-                    <li><a href="#work">Work</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#intro">CONTACT</a></li>
+                    <li><a href="#work">PROJECT</a></li>
+                    <li><a href="#about">BLOG</a></li>
+                    {/* <li><a href="#contact">Contact</a></li> */}
                     {/* <li><a href="#elements">Elements</a></li> */}
                 </ul>
             </nav>
