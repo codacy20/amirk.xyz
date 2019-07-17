5455bf49d2d9303793b7abc3ac1568731e85ced4
codacy20 <kmuc72@gmail.com>
2019-07-17 17:04:44 +0200

    main body and content component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Footer from './Footer'
 import Header from './Header'
+import Main from './Main'
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <Header />
+      <Main />
       <Footer />
     </React.Fragment>
   );
